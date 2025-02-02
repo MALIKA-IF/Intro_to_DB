2128c1157f4eb558761f309bdfb91fbadd77fa3e
Malika Edaif <edmalika957@gmail.com>
2025-02-02 19:39:53 +0100

    create file task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -15,7 +15,7 @@ except mysql.connector.Error:
 
 mycursor = mydb.cursor()
 # Execute SQL statements using the execute() method on the cursor
-mycursor.execute("USE alx_book_store")
+mycursor.execute("USE alx_book_store;")
 mycursor.execute("INSERT INTO Customers(customer_id,customer_name,email,address) 
 VALUES (1,'Cole Baidoo','cbaidoo@sandtech.com','123 Happiness Ave.'), 
 (2,'Blessing Malik','bmalik@sandtech.com','124 Happiness Ave.'),
