0a296438446f9166937e936883ee3c916c2c0a3b
Malika Edaif <edmalika957@gmail.com>
2025-02-02 19:00:22 +0100

    create file task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -16,7 +16,7 @@ except mysql.connector.Error:
 mycursor = mydb.cursor()
 # Execute SQL statements using the execute() method on the cursor
 mycursor.execute("USE alx_book_store;")
-mycursor.execute("INFORMATION_SCHEMA.COLUMNS", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books';")
+#mycursor.execute("INFORMATION_SCHEMA.COLUMNS", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books';")
 
 # Close connection to the databasse  
 mycursor.close()
