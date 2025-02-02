997201bbc8618926280bfc65e3340b9d47c09264
Malika Edaif <edmalika957@gmail.com>
2025-02-02 19:33:32 +0100

    create file task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -20,7 +20,7 @@ mycursor.execute("INSERT INTO customer (customer_id,customer_name,email,address)
 VALUES (1,"Cole Baidoo","cbaidoo@sandtech.com","123 Happiness Ave."), 
 (2,"Blessing Malik","bmalik@sandtech.com","124 Happiness Ave."),
 (3,"Obed Ehoneah","eobed@sandtech.com","125 Happiness Ave."),
-(4,"Nehemial Kamolu","nkamolu@sandtech.com","126 Happiness Ave.")")
+(4,"Nehemial Kamolu","nkamolu@sandtech.com","126 Happiness Ave.")";)
 
 # Close connection to the databasse  
 mycursor.close()
