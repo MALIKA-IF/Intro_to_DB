c72355ddec9454172617c053975d2eae1346db9d
Malika Edaif <edmalika957@gmail.com>
2025-02-02 18:38:32 +0100

    create file task_3.sql

---
+++ task_3.sql
@@ -0,0 +1,22 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="MalikaEdaif@1995",
+    database="alx_book_store"
+)
+
+try:
+    print("Database 'alx_book_store' created successfully!")
+
+except mysql.connector.Error:
+    print("Error")
+
+mycursor = mydb.cursor()
+# Execute SQL statements using the execute() method on the cursor
+mycursor.execute("SHOW TABLES;")
+
+# Close connection to the databasse  
+mycursor.close()
+mydb.close()
