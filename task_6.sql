a32bc77a5894ca037523453ad60e41211a75a94e
Malika Edaif <edmalika957@gmail.com>
2025-02-02 20:00:10 +0100

    create file task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -17,8 +17,7 @@ mycursor = mydb.cursor()
 # Execute SQL statements using the execute() method on the cursor
 mycursor.execute("USE alx_book_store;")
 mycursor.execute("INSERT INTO customer(customer_id,customer_name,email,address) 
-VALUES(1,'Cole Baidoo','cbaidoo@sandtech.com','123 Happiness Ave.'), 
-(2,'Blessing Malik','bmalik@sandtech.com','124 Happiness Ave.'),
+VALUES(2,'Blessing Malik','bmalik@sandtech.com','124 Happiness Ave.'),
 (3,'Obed Ehoneah','eobed@sandtech.com','125 Happiness Ave.'),
 (4,'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness Ave.');")
 
