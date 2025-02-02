c08a0e29700e00e17667131d019a28c5f41487a4
Malika Edaif <edmalika957@gmail.com>
2025-02-02 20:05:04 +0100

    create file task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -17,11 +17,11 @@ mycursor = mydb.cursor()
 # Execute SQL statements using the execute() method on the cursor
 mycursor.execute("USE alx_book_store;")
 mycursor.execute("INSERT INTO customer(customer_id,customer_name,email,address) 
-VALUES(2,Blessing Malik,bmalik@sandtech.com,124 Happiness Ave.)")
+VALUES(2,Blessing Malik,bmalik@sandtech.com,124 Happiness Ave.);")
 mycursor.execute("INSERT INTO customer(customer_id,customer_name,email,address) 
-VALUES(3,Obed Ehoneah,eobed@sandtech.com,125 Happiness Ave.)")
+VALUES(3,Obed Ehoneah,eobed@sandtech.com,125 Happiness Ave.);")
 mycursor.execute("INSERT INTO customer(customer_id,customer_name,email,address) 
-VALUES(4,Nehemial Kamolu,nkamolu@sandtech.com,126 Happiness Ave.)")
+VALUES(4,Nehemial Kamolu,nkamolu@sandtech.com,126 Happiness Ave.);")
 
 
 # Close connection to the databasse  
